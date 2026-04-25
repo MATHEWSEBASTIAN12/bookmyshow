d57c1a26cc06e076377b0cf4f623c79627c76aaa
MATHEWSEBASTIAN12 <mathewksebastian12@gmail.com>
2026-04-25 12:54:02 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,11 +1,1 @@
-import { db } from "./firebase.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-window.addMovie = async () => {
-  await addDoc(collection(db, "movies"), {
-    name: name.value,
-    img: img.value
-  });
-
-  alert("Movie Added");
-};
